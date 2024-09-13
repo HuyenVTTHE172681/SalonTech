d298caca6241ffc1c274088e48e641e71973db25
Huyen Vu <huyenvtthe172681@fpt.edu.vn>
2024-09-13 11:25:00 +0700

    user model

--- src/app/model/user.ts
+++ src/app/model/user.ts
@@ -1,14 +1,18 @@
-export interface User {
-  _id: string;
-  name: string;
-  phone: string;
-  email: string;
-  address: string;
-  identifier: string;
-  birthday: string; // Use string type for ISO 8601 date format
-  userName: string;
-  status: number;
-  roles?: string; // Optional field
+export class User {
+  _id: string = '';
+  name: string = '';
+  phone: string = '';
+  email: string = '';
+  address: string = '';
+  city: string = '';
+  district: string = '';
+  commune: string = '';
+  identifier: string = '';
+  birthday: string = ''; // Use string type for ISO 8601 date format
+  userName: string = '';
+  password: string = '';
+  status: number = 0;
+  roles?: string[]; // Optional field
   avatar?: string; // Optional field
   cover?: string; // Optional field
   customer_id?: string; // Optional field
